df8aae817d983bb4e623fa863fc6d82edd2788cd
reeyarn <reeyarn@gmail.com>
2025-12-22 07:12:47 +0100

    sync
    changed a local folder

---
+++ openesef/old/20250307___init__.py
@@ -0,0 +1,15 @@
+"""OpenESEF package"""
+
+from .version import __version__,  PICKLE_VERSION, PARQUET_VERSION
+
+# Import subpackages
+from . import base
+from . import taxonomy
+from . import instance
+from . import ixbrl
+from . import engines
+from . import util
+from . import edgar
+from . import filings_xbrl_org
+# Don't import resolver directly here
+# Remove: from . import resolver
